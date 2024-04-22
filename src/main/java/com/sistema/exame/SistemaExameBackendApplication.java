7a84fdf0fef2386b0d98795df6e02cad68130246
agsilvamhm <adalberto.goncalves.silva@gmail.com>
2024-04-22 20:58:19 -0300

    Primeiro commit - estrutura do projeto

---
+++ src/main/java/com/sistema/exame/SistemaExameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.exame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaExameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaExameBackendApplication.class, args);
+	}
+
+}
